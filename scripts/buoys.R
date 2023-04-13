d883faf390c438e37234bd4567b4e563d811b797
Ben Tupper <btupper@bigelow.org>
2023-04-13 10:43:58 -0400

    populate setup, add data/buoy_listing.csv

--- scripts/buoys.R
+++ scripts/buoys.R
@@ -1,1 +1,17 @@
 # Accessing NERACOOS buoy data
+
+#' Returns a table of buoy metadata
+#' 
+#' @return tibble
+buoy_lut = function(){
+  vars = "wtemp, wsal, curspeed, curdir, atemp, chlor, airspeed, airdir, gusts"
+  depths = "any"
+  dplyr::tribble(
+    ~name, ~longname, ~id, ~lon, ~lat, 
+    "wms", "Western Maine Shelf", "B01", NA_real_, NA_real_, 
+    "cms", "Central Maine Shelf", "E01" ,NA_real_, NA_real_, 
+    "penbay", "Penobscot Bay", "F01", NA_real_, NA_real_, 
+    "ems", "Eastern Maine Shelf",  "I01", NA_real_, NA_real_, 
+    "jb", "Jordan Basin", "M01", NA_real_, NA_real_, 
+    "nec", "Northeast Channel", "N01", NA_real_, NA_real_)
+}
--- setup.R
+++ setup.R
@@ -1,6 +1,15 @@
 suppressPackageStartupMessages({
+  here::i_am("setup.R")
+  library(here)
+  library(rerddap)
   library(ggplot2)
   library(tidyr)
   library(readr)
   library(dplyr)
 })
+
+# source functionality
+for (f in list.files(here("scripts"), pattern = "^.*\\.R$", full.names = TRUE)){
+  source(f)
+}
+
