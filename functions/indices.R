195b3ebc3ac4692953ac3b5b823fc12f09d508cb
jevanilla <jevanilla@bigelow.org>
2023-07-06 10:01:33 -0700

    Added GSI; updated climate index functions to aggregate annually and export a wide table of all indices

--- functions/indices.R
+++ functions/indices.R
@@ -40,6 +40,13 @@ fetch_nao <- function(form = c("long", "wide")[1]) {
   return(x)
 }
 
+#' A wrapper around \code fetch_nao()
+#' 
+#' @return a tibble of nao data
+read_nao <- function(...) {
+  fetch_nao(...)
+}
+
 
 #' Reads the Atlantic Multidecadal Oscillation (AMO) Index data
 #' AMO unsmoothed from the Kaplan SST V2                                                                 
@@ -84,3 +91,115 @@ fetch_amo <- function(form = c("long", "wide")[1]) {
   return(x)
   
 }
+
+#' A wrapper around \code fetch_amo()
+#' 
+#' @return a tibble of amo
+read_amo <- function(...) {
+  fetch_amo(...)
+}
+
+#' Reads in the locally stored gulf stream index (GSI)
+#' provided by Chen et al
+#' 
+#' @param filename character filename of which GSI to read
+#' @param path character path to the climate data directory
+#' @param form character defining form to return data table long or wide
+#' @return a tibble of gsi data 
+#' 
+#' @export
+read_gsi <- function(filename = c("Chen_EN4_T200_GSI_1954_2021_monthly.xlsx", "Chen_SSH-based_GSI_monthly_199301-202112.xlsx")[1],
+                     path = here::here("data", "climate"),
+                     form = c("long", "wide")[1]) {
+  
+  file <- file.path(path, filename)
+  
+  x <- readxl::read_xlsx(file) |>
+    #dplyr::mutate(date = sprintf("%0.2f.01", .data$Month) |> as.Date(format = "%Y.%m.%d"), .before = .data$GSI) |>
+    dplyr::mutate(date = sprintf("%0.2f.01", .data$Month) |> as.Date(format = "%Y.%m.%d"), .before = dplyr::all_of("GSI")) |>
+    dplyr::select(-"Month") |>
+    dplyr::rename(gsi = dplyr::all_of("GSI"))
+  
+  if (form == "wide") {
+    
+    x <- x |>
+      dplyr::mutate(year = as.numeric(format(date, format="%Y")),
+                    month = tolower(format(date, format="%b")),
+                    .before = .data$gsi)
+      tidyr::pivot_wider(id_cols = .data$year,
+                         names_from = .data$month,
+                         values_from = .data$gsi)
+  }
+  
+  return(x)
+}
+
+
+#' Aggregate climate index data by month or year
+#' 
+#' @param x a tibble of climate index data
+#' @param by character the interval over which to aggregate - one of 'month' or 'year'
+#' @retun a tibble of climate index data
+#' 
+#' 
+aggregate_climate_index <- function(x, 
+                            by = c("month", "year")[2]) {
+  
+  if (by == "month") {
+    return(x)
+  } else if (by == "year") {
+    x |>
+      dplyr::group_by(format(date, format="%Y")) |>
+      dplyr::group_map(
+        function(tbl, key) {
+          v <- sixnum(tbl |> dplyr::pull()) |>
+            as.list() |>
+            dplyr::as_tibble()
+          v |>
+            #dplyr::mutate(date = as.Date(format(tbl[1,]$date, format="%Y-01-01")), .before = dplyr::all_of("min")) |>
+            dplyr::mutate(date = as.Date(format(tbl[1,]$date, format="%Y-01-01")), .before = dplyr::all_of("min"))
+        }
+      ) |>
+      dplyr::bind_rows()
+  } else {
+    stop("argument by can only be 'month' or 'year'")
+  }
+  
+}
+
+
+#' Join aggregated climate index data into one wide table
+#' 
+#' @param complete_intervals logical if TRUE only returns years that do not have NA values
+#' @return a wide table of aggregated climate index data
+#' 
+export_climate_indices <- function(complete_intervals = TRUE) {
+  
+  amo <- read_amo() |>
+    aggregate_climate_index() |>
+    dplyr::mutate(index = "amo")
+  
+  nao <- read_nao() |>
+    aggregate_climate_index() |>
+    dplyr::mutate(index = "nao")
+  
+  gsi <- read_gsi() |>
+    aggregate_climate_index() |>
+    dplyr::mutate(index = "gsi")
+  
+  r <- bind_rows(amo, nao, gsi) |>
+    tidyr::pivot_wider(id_cols = dplyr::all_of("date"),
+                       names_from = "index",
+                       names_glue = "{index}.{.value}",
+                       values_from = where(is.numeric))
+  
+  if (complete_intervals) {
+    r <- r |>
+      tidyr::drop_na()
+  }
+  
+  return(r)
+  
+}
+
+
