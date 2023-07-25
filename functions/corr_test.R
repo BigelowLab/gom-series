ef0f4469ae89324e426efce3cabc76ac14c8b543
jevanilla <jevanilla@bigelow.org>
2023-07-25 10:55:13 -0700

    Updated correlation test with analysis/target variables; updated colors on network plot

--- functions/corr_test.R
+++ functions/corr_test.R
@@ -1,15 +1,21 @@
 #' Make a network plot with multiple sources of data
 #' 
 #' @param x tibble of data from `read_export()`
-#' @param include character vector of keywords to select data sources
+#' @param include character vector of variable names
+#' @param start_year integer year to filter data going into network plot
 #' @return a network plot
 #' 
-network <- function(x, 
-                    include = c("nao", "amo", "gsi")) {
+network <- function(x = read_export(), 
+                    include = analysis_vars(),
+                    start_year = 1970) {
+  
+  start <- sprintf("%s-01-01", start_year)
   
   x |>
-    dplyr::select(dplyr::contains(include)) |>
+    dplyr::filter(date >= as.Date(start)) |>
+    dplyr::select(dplyr::all_of(include)) |>
+    dplyr::select(dplyr::where(~abs(sum(., na.rm=TRUE)) > 0)) |>
     corrr::correlate() |>
-    corrr::network_plot()
+    corrr::network_plot(colours = c("skyblue1", "white", "indianred2"))
   
 }
